25f330a8014c0dd8a7469d63f9cb52fe56f49b3e
crea <masataka0504@gmail.com>
2020-05-01 16:55:43 +0900

    Initial Commit

---
+++ CombineInPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineInPractice
+//
+//  Created by crea on 2020/05/01.
+//  Copyright © 2020 crea. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
